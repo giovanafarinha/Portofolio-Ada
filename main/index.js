b8386fae2b649f1d207b30afe99a98609d114537
Giovana Farinha <giovanafarinha8@gmail.com>
2025-10-23 10:07:42 +0200

    Added more struture into my HTML

---
+++ main/index.js
@@ -0,0 +1,8 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+const menuCloseButton = document.querySelector("#menu-close-button");
+menuCloseButton.addEventListener("click", () => {
+  menuOpenButton.click();
+});
